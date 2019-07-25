697396d21fbe6e1cf78d00670830bd910c98edbf
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-25 12:38:53 -0400

    Add project files.

---
+++ Interface.Proj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Interface.Proj {
+    class Program : IStudents {                    // ": IStudents" implements interface
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+        public Student[] GetStudents() {
+            return new Student[0];
+        }
+    }
+    class Student { }
+    interface IStudents {               // must start with a capital I  /  Allows you and others to use at same time
+        Student[] GetStudents();
+            
+    }
+}
